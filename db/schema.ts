866e785ab7b379f305689cff73fe05e7aad885c4
owr7 <92971363+0WR7@users.noreply.github.com>
2026-05-08 18:38:21 +0300

    Update schema.ts

--- db/schema.ts
+++ db/schema.ts
@@ -1,12 +1,13 @@
 import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
 
+//change id to be not null later
 export const blogs = pgTable("blogs", {
     id: serial("id").primaryKey(),
     title: text("title").notNull(),
     author: text("author").notNull(),
     url: text("url").notNull(),
     likes: integer().default(0).notNull(),
-    userId: integer("user_id").notNull().references(() => users.id)
+    userId: integer("user_id").references(() => users.id)
 });
 
 export const users = pgTable("users", {
